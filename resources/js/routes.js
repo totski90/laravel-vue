d3f4c0ca10169e20ca96086cd2459f34464c4ed0
vic datu andam <vica@fligno.com.ph>
2019-01-16 13:52:41 +0800

    Vue SPA Essentials: Routing

---
+++ resources/js/routes.js
@@ -0,0 +1,30 @@
+
+
+import VueRouter from 'vue-router';
+
+
+let routes = [
+	
+	{
+
+		path: '/',
+
+		component: require('./views/Home.vue')
+
+	},
+
+	{
+
+		path: '/about',
+
+		component: require('./views/About.vue')
+
+	}
+
+];
+
+export default new VueRouter({
+
+	routes
+
+});
